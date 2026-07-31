d2a961ac71c6ed1969a5670cda02c12abba9cc87
Melf Kammholz <melf.kammholz@gmail.com>
2026-07-31 06:53:24 +0200

    index data declarations

--- src/index.py
+++ src/index.py
@@ -3,11 +3,11 @@ import re
 from sys import maxsize
 import json
 
-# TODO add Haskell's data and newtype
 
 SOURCE_DIR = Path(__file__).parent
 CODE_SOURCE_DIR = SOURCE_DIR / 'code/src'
-SIGNATURE_PATTERN = re.compile(r'^([^\s]*)?\s*::')
+FUNCTION_PATTERN = re.compile(r'^([^\s]*)?\s*::')
+DATA_PATTERN = re.compile(r'^(?:data|newtype)\s+([A-Z][a-z0-9_\']*)')
 
 EXCLUDE_FILES = [
   'FFT.hs'
@@ -17,25 +17,35 @@ EXCLUDE_FILES = [CODE_SOURCE_DIR / file for file in EXCLUDE_FILES]
 
 index = {}
 
+# Current indexing does not disambiguate between declaration in different 
+# modules. In the document, templates are picked based on the functions that
+# are cited in a test or challenge. If ambiguous declaration are removed and
+# there is at least one unique declaration, the problem does not occur.
+bad_funs = []
+
 for file in CODE_SOURCE_DIR.iterdir():
   if file.is_file() and file.name.endswith('.hs') and file not in EXCLUDE_FILES:
     with open(file, encoding='utf-8') as handle:
       file = str(file.relative_to(CODE_SOURCE_DIR))
       for i, line in zip(range(1, maxsize), handle.readlines()):
-        if matches := SIGNATURE_PATTERN.match(line):
+        if matches := FUNCTION_PATTERN.match(line) or DATA_PATTERN.match(line):
           fun = matches.group(1)
-          if fun in index:
+          if fun in index and index[fun]['file'] != file:
             print(
-              f"Duplicate definition of function '{fun}'. "
+              f"Duplicate declaration of function or datatype '{fun}'. "
               f"Previously defined at {index[fun]['file']}:{index[fun]['line']}. "
               f"Redefined at {file}:{i}."
             )
+            bad_funs.append(fun)
           else:
             index[fun] = {
               'file': file,
               'line': i
             }
-                    
+
+for fun in bad_funs:
+    del index[fun]
+
 with open(SOURCE_DIR / 'index.json', 'w') as handle:
   json.dump(index, handle, indent='  ')
 
